1f86871bae5fd12bc129ded32aff8d4da319bdba
Aleksandr Barmin <barmin.alexander@gmail.com>
2021-07-12 12:54:27 +0300

    [Issue-33] Adding examples of transactional messages sending and processing

---
+++ microservices/microservice-delivery/src/main/java/com/epam/training/service/delivery/service/RecipeProcessor.java
@@ -0,0 +1,32 @@
+package com.epam.training.service.delivery.service;
+
+import com.epam.training.service.delivery.model.ProcessedRecipe;
+import com.epam.training.service.delivery.model.message.RecipeModel;
+import com.epam.training.service.delivery.repository.ProcessedRecipeRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RecipeProcessor {
+  @Autowired
+  private ProcessedRecipeRepository repository;
+
+  public boolean process(RecipeModel recipeModel) {
+    final Optional<ProcessedRecipe> processedOptional = repository.findProcessedRecipeByRecipeId(recipeModel.getId());
+    if (processedOptional.isPresent()) {
+      // do nothing, this recipe has already been processed
+      return false;
+    }
+    final ProcessedRecipe processed = new ProcessedRecipe();
+    processed.setRecipeId(recipeModel.getId());
+
+    repository.save(processed);
+
+    System.out.printf("Do some real processing");
+
+    return true;
+  }
+
+
+}
--- microservices/microservice-recipies/src/main/java/com/epam/training/microservice/service/recipes/service/recipe/RecipeService.java
+++ microservices/microservice-recipies/src/main/java/com/epam/training/microservice/service/recipes/service/recipe/RecipeService.java
@@ -1,6 +1,7 @@
 package com.epam.training.microservice.service.recipes.service.recipe;
 
 import com.epam.training.microservice.service.recipes.model.Doctor;
+import com.epam.training.microservice.service.recipes.model.OutgoingRecipe;
 import com.epam.training.microservice.service.recipes.model.Recipe;
 import com.epam.training.microservice.service.recipes.model.Recipient;
 import com.epam.training.microservice.service.recipes.repository.RecipeRepository;
@@ -13,6 +14,8 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
+import static com.google.common.base.Preconditions.*;
+
 import java.time.LocalDate;
 import java.util.Optional;
 
@@ -22,13 +25,21 @@ public class RecipeService {
     private RecipeRepository recipeRepository;
 
     @Autowired
-    private StreamBridge streamBridge;
+    private OutgoingRecipeService outgoingRecipeService;
+
+//    @Autowired
+//    private StreamBridge streamBridge;
 
     public Recipe save(Recipe recipe) {
-        final Message<Recipe> message = MessageBuilder.withPayload(recipe).build();
-        streamBridge.send("deliveryNotificator-out-0", message);
+//        final Message<Recipe> message = MessageBuilder.withPayload(recipe).build();
+//        streamBridge.send("deliveryNotificator-out-0", message);
 
-        return recipeRepository.save(recipe);
+        final Recipe savedRecipe = recipeRepository.save(recipe);
+
+        final OutgoingRecipe outgoingRecipe = outgoingRecipeService.findOrCreate(savedRecipe);
+        checkArgument(outgoingRecipe != null, "Outgoing recipe wasn't created");
+
+        return savedRecipe;
     }
 
     public Optional<Recipe> find(Doctor doctor,
