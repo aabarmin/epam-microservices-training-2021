810376566643c77963c094774daf9be95d414429
Aleksandr Barmin <barmin.alexander@gmail.com>
2021-08-01 18:24:40 +0300

    #43 Rewriting everything with Gradle
    Add adding more examples of GraphQL

--- microservices/microservice-graphql/src/main/java/com/epam/training/miservices/services/graphql/GraphQLApplication.java
+++ microservices/microservice-graphql/src/main/java/com/epam/training/miservices/services/graphql/GraphQLApplication.java
@@ -2,7 +2,9 @@ package com.epam.training.miservices.services.graphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class GraphQLApplication {
   public static void main(String[] args) {
